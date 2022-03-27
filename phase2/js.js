e7b37e07a33b02b4a04b4106877d31059d463199
Amanda Le <amandale@terpmail.umd.edu>
2022-03-26 22:29:29 -0400

    added json parsing for recipes

--- phase2/js.js
+++ phase2/js.js
@@ -10,9 +10,17 @@ let allergenInformation = new Array();
 // level of recipes
 let level = new String();
 
-let recipesJson = '[{ "name": "Cereal", "info": ["easy", "vegetarian", "gluten-free"], "ingredients": [{"quantity": "2 cups","name": " cereal"},{"quantity": "2 cups","name": "milk"} ],  "steps": ["Put cereal of choosing into bowl.","pour milk in bowl and enjoy."],"imageURL": "cereal.jpeg"}, { "name": "Cereal","ingredients": [{"quantity": "1","name": " beef roast", "type": "Meat"}]} ]';
+//let recipesJson = '[{ "name": "Cereal", "info": ["easy", "vegetarian", "gluten-free"], "ingredients":[{"quantity": "2 cups","name": " cereal"},{"quantity": "2 cups","name": "milk"} ],"steps": ["Put cereal of choosing into bowl.","pour milk in bowl and enjoy."],"imageURL": "cereal.jpeg"},{ "name": "Cereal","ingredients": [{"quantity": "1","name": " beef roast", "type": "Meat"}]} ]';
 //for the json data to be read in 
-var data;
+
+let recipes;
+fetch("./recipe.json").then(
+        function(u){ return u.json();}
+      ).then(
+        function(json){
+          recipes = json;
+        }
+      )
 
 function main(){
 
@@ -22,20 +30,6 @@ function main(){
 
     // for the recipe form
     document.getElementById("processRecipeButton").onclick = processRecipeData;
-
-    //read in the recipe json file
-    // fetch('Recipes.json')
-    // .then(function (response) {
-    //   return response.json();
-    // })
-    // .then(function (data) {
-    //   appendData(data);
-    // })
-    // .catch(function (err) {
-    //   console.log(err);
-    // });
-    data = JSON.parse(recipesJson);
-    //console.log(data[0].name);
 
 }
 
@@ -54,20 +48,16 @@ function processRecipeData(){
     }
     var levelSelected = document.getElementById("levelSelection");
     level = levelSelected.options[levelSelected.selectedIndex].text
-    // console.log(level);
-    // console.log(allergenInformation);
-    // alert("your level chosen is " + level + " allergen information is " + allergenInformation);
     showRecipe();
 }
+
 //function to display the recipe taken based on options/levels from json
 function showRecipe(){
     //remove the form??
 
-    // var container = document.getElementById("recipe");
-
     //find recipe (just using the first one for now)
     //can just iterate through the recipe's info to find satisfying recipe
-    recipe = data[0];
+    recipe = recipes[0];
 
     //display found recipe
     var divName =  document.getElementById("recipeName");
@@ -80,36 +70,13 @@ function showRecipe(){
         divInfo.innerHTML += ' ';
     }
     
-
-    //how to add an img?
     var divImg = document.getElementById("recipeImage");
     img = document.createElement("img");
     img.src = recipe.imageURL;
     divImg.appendChild(img);
 
-    //how to add a list
-    // var divIngredients = document.createElement("div");
-    // var ingredientsList = document.createElement("ol")
-
-    // for (var i = 0; i < recipe.ingredients.length; i++) {
-    //     var li = document.createElement("li");
-    //     li.innerText = recipe.ingredients[i].name
-    //     ingredientsList.appendChild(li);
-    // }
-    // divIngredients.appendChild(ingredientsList);
-    // var ol = document.createElement("ol");
-    // var ing = recipe.ingredients;
-    // for (var i = 0; i < ing.length; i++) {
-    //     var li = document.createElement("li");
-    //     var checkBox = document.createTextNode(recipe.ingredients[i].name);
-    //     checkBox.type = "checkbox";
-    //     li.appendChild(checkBox);
-    //     ol.appendChild(li);
-    // }
-
     let list = document.getElementById("ingredientsList");
     var ing = recipe.ingredients;
-    // console.log(ing[0].name);
     ing.forEach((item)=>{
         let li = document.createElement("li");
         li.innerText = item.name;
@@ -119,17 +86,11 @@ function showRecipe(){
 
         var button = document.createElement("button");
         button.innerHTML = "add to shopping list";
-        button.onclick = addToShoppingList(item.name);
+        button.addEventListener("click",  function() {addToShoppingList(item.name)});
         console.log(shoppingList);
         li.appendChild(button);
         list.appendChild(li);
       })
-    // for (var i = 0; i < ing.length; i++) {
-    //     var li = document.createElement("li");
-    //     li.innerText = ing[i].name;
-    //     list.appendChild(li);
-    // }
-
 
     var divInstructions = document.getElementById("instructionsList");
     var instructs = recipe.steps;
@@ -141,16 +102,12 @@ function showRecipe(){
         li.appendChild(checkBox);
         list.appendChild(li);
       })
-
-
-
-
 }
-
 
 //function to add the item to shoppingList
 function addToShoppingList(item){
     shoppingList.push(item);
+    //console.log(shoppingList);
 }
 
 //function to add item to inventory
