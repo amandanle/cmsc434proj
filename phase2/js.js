dc7ba5539e2d3f684ecc6ad1a3fea9e7034b5ece
Amanda Le <amandale@terpmail.umd.edu>
2022-03-28 16:04:08 -0400

    reverted

--- phase2/js.js
+++ phase2/js.js
@@ -1,14 +1,26 @@
 
 
-// our global variables to store things, just using maps or arrays but feel free to change it
+// our global variables to store things, just using arrays but feel free to change it to maps if want to
 // map item in shopping list to cost or how many or store (if we are doing that)
-let shoppingList = new Map();
+let shoppingList = new Array();
 //map item in inventory to how much or date bought (if we are doing that)
-let inventory = new Map();
+let inventory = new Array();
 //allergen info for recipes.
 let allergenInformation = new Array();
 // level of recipes
 let level = new String();
+
+//let recipesJson = '[{ "name": "Cereal", "info": ["easy", "vegetarian", "gluten-free"], "ingredients":[{"quantity": "2 cups","name": " cereal"},{"quantity": "2 cups","name": "milk"} ],"steps": ["Put cereal of choosing into bowl.","pour milk in bowl and enjoy."],"imageURL": "cereal.jpeg"},{ "name": "Cereal","ingredients": [{"quantity": "1","name": " beef roast", "type": "Meat"}]} ]';
+//for the json data to be read in 
+
+let recipes;
+fetch("./recipe.json").then(
+        function(u){ return u.json();}
+      ).then(
+        function(json){
+          recipes = json;
+        }
+      )
 
 function main(){
 
@@ -36,9 +48,69 @@ function processRecipeData(){
     }
     var levelSelected = document.getElementById("levelSelection");
     level = levelSelected.options[levelSelected.selectedIndex].text
-    console.log(level);
-    console.log(allergenInformation);
-    alert("your level chosen is " + level + " allergen information is " + allergenInformation);
-//    alert("your allergen information is " + allergenInformation);
+    showRecipe();
+}
 
+//function to display the recipe taken based on options/levels from json
+function showRecipe(){
+    //remove the form??
+
+    //find recipe (just using the first one for now)
+    //can just iterate through the recipe's info to find satisfying recipe
+    recipe = recipes[0];
+
+    //display found recipe
+    var divName =  document.getElementById("recipeName");
+    divName.innerHTML = recipe.name;
+
+    var divInfo = document.getElementById("recipeInfo");
+    var info = recipe.info;
+    for (var i = 0; i < info.length; i++) {
+        divInfo.innerHTML += info[i];
+        divInfo.innerHTML += ' ';
+    }
+    
+    var divImg = document.getElementById("recipeImage");
+    img = document.createElement("img");
+    img.src = recipe.imageURL;
+    divImg.appendChild(img);
+
+    let list = document.getElementById("ingredientsList");
+    var ing = recipe.ingredients;
+    ing.forEach((item)=>{
+        let li = document.createElement("li");
+        li.innerText = item.name;
+        var checkBox = document.createElement('input');
+        checkBox.type = "checkbox";
+        li.appendChild(checkBox);
+
+        var button = document.createElement("button");
+        button.innerHTML = "add to shopping list";
+        button.addEventListener("click",  function() {addToShoppingList(item.name)});
+        console.log(shoppingList);
+        li.appendChild(button);
+        list.appendChild(li);
+      })
+
+    var divInstructions = document.getElementById("instructionsList");
+    var instructs = recipe.steps;
+    instructs.forEach((item)=>{
+        let li = document.createElement("li");
+        li.innerText = item;
+        var checkBox = document.createElement('input');
+        checkBox.type = "checkbox";
+        li.appendChild(checkBox);
+        list.appendChild(li);
+      })
+}
+
+//function to add the item to shoppingList
+function addToShoppingList(item){
+    shoppingList.push(item);
+    //console.log(shoppingList);
+}
+
+//function to add item to inventory
+function addToInventory(item){
+    inventory.push(item);
 }
